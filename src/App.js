30b28ac1103dabd536c1c0cd9064ae45273f3064
AnCial <aguinaldo.marcial1@gmail.com>
2024-07-28 18:24:15 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/index.css";
+import { useEffect, useState } from "react";
+import Divider from "./images/pattern-divider-desktop.svg";
+import Icon from "./images/icon-dice.svg";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Advice />
     </div>
   );
 }
 
-export default App;
+function Advice() {
+  const [advice, setAdvice] = useState("");
+  const [randomInt, setRandomInt] = useState("");
+
+  async function fetchAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setAdvice(data.slip.advice);
+  }
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function handleClick() {
+    const random = getRandomInt(1, 217);
+    setRandomInt(random);
+    fetchAdvice();
+  }
+
+  return (
+    <div className="advice">
+      <AdviceComponent
+        advice={advice}
+        onGetRandomInt={getRandomInt}
+        randomInt={randomInt}
+      />
+      <Button onHandleClick={handleClick} />
+    </div>
+  );
+}
+
+function AdviceComponent({ advice, randomInt }) {
+  return (
+    <div className="advice__text">
+      <span>advice #{randomInt}</span>
+      <h2>"{advice}"</h2>
+
+      <img src={Divider} alt="divider" />
+    </div>
+  );
+}
+function Button({ onHandleClick }) {
+  return (
+    <button onClick={onHandleClick}>
+      <img src={Icon} alt="icon" />
+    </button>
+  );
+}
